bdecbd3bad514bdaab6cca825d067ed6b86dae08
Jacob Anderson <jwanderson@unomaha.edu>
2019-10-18 11:48:50 -0500

    Fsfsf

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react'
 
 let quizData = require('./quiz_data.json')
 
-class Quiz extends React.Component {
+class Quiz extends Component {
 
     render() {
-
+        <div>
+            Quiz
+        </div>
     }
-    
-    export default Quiz
 }
+
+export default Quiz
